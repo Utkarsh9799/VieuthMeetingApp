de65bee8fc599b1cf3663c442ca6878665252e31
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-06-14 21:49:09 +0530

    Fetching meetings

--- meetings-app/src/components/Meetings.js
+++ meetings-app/src/components/Meetings.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 function Meetings() {
 	const [state, setstate] = useState([]);
@@ -10,6 +10,30 @@ function Meetings() {
 		String(new Date().getMonth() + 1).padStart(2, '0')
 	);
 	const [userYear, setYear] = useState(new Date().getFullYear());
+
+	useEffect(() => {
+		fetchMeetings();
+	}, [userDate, userMonth, userYear]);
+
+	const fetchMeetings = async () => {
+		console.log(userDate);
+		try {
+			const res = await axios.get(
+				`api/schedule?date=${userDate}/${userMonth}/${userYear}`
+			);
+			console.log(res);
+			const sorted = res.data.sort((a, b) => {
+				var t1 = a.start_time.replace(':', '');
+				var t2 = b.start_time.replace(':', '');
+				// console.log(t1)
+				return t1 - t2;
+			});
+			// console.log(sorted)
+			setstate(sorted);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
 	return (
 		<div>
